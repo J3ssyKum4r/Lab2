810c68027cd0dfbcc948a7bb8eb805d115e617ca
jessy <jessysk.25@ichat.sp.edu.sg>
2026-05-07 11:15:19 +0800

    update changes

---
+++ bmi.py
@@ -0,0 +1,20 @@
+def calculate_bmi(height,weight):
+    print("Height=" + str(height))
+    print("Weight=" + str(weight))
+
+    bmi = weight / (height **2)
+
+    print(f"Your BMI is {bmi} ")
+
+    if bmi < 18.5 :
+        print("Under Weight")
+        return -1
+    elif 18.5 <= bmi <= 25.0:
+        print("Normal Weight")
+        return 0
+    else:
+        print("Over Weight")
+        return 1
+
+calculate_bmi(weight=57, height=1.73)
+
